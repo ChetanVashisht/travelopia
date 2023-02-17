fb4ab7b55b5a092ae6339aa3352581c066eae827
Chetan Vashisht <chetanvashisht@Chetans-Mac-mini.local>
2023-02-17 18:45:12 +0530

    Got the routing working

---
+++ front-end/src/components/TravelForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TravelForm() {
+    return (
+        <div>
+            <h1>Travel Form</h1>
+        </div>
+    )
+}
---
+++ front-end/src/components/Travellers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Travellers() {
+    return (
+        <div>
+            <h1>List of Travellers</h1>
+        </div>
+    )
+}
