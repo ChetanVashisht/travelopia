46c064d8fbe276508a2d0f0cbd6d6707a922d5fb
Chetan Vashisht <chetanvashisht@Chetans-Mac-mini.local>
2023-02-19 17:11:29 +0530

    Updated the front-end to consume the api

--- front-end/src/components/Travellers.jsx
+++ front-end/src/components/Travellers.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import sampleData from '../assets/sampleData'
 import Traveller from './Traveller'
 
 export default function Travellers() {
     const [data, setData] = useState([])
-
-    const apiCall = () => setData(sampleData)
     const renderTraveller = (traveller, i) => (<Traveller traveller={traveller} key={i} />)
-    useEffect(apiCall, [])
+
+    const loadTravellers = () => {
+        fetch("http://localhost:8000/travellers")
+            .then(resp => resp.json())
+            .then(setData)
+    }
+    useEffect(loadTravellers, [])
+
+
     return (
         <section>
             <h1>List of Travellers</h1>
